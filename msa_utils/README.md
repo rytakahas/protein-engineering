1927c102dc6d5cee37704a06a1e54193571631ce
Ryoji Takahashi <ryoji.takahashi@gmail.com>
2025-11-07 15:20:29 +0100

    refactor: monorepo layout (packages/{rescontact,resintnet,seqml}) and skeleton dirs

--- msa_utils/README.md
+++
@@ -1,9 +0,0 @@
-# DeepMSA Docker Utility
-
-This directory contains a Docker-based setup to run DeepMSA locally.
-
-## Usage
-
-```bash
-bash run_deepmsa.sh
-
--- msa_utils/run_deepmsa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# msa_utils/run_deepmsa.sh
-
-docker build -t deepmsa ./msa_utils
-docker run -it --rm \
-  -v $(pwd)/data:/workspace/data \
-  deepmsa
-
